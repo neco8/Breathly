a5d13f8883eb888dd3bc311317dea77c97632916
neco8 <shogibeshow@gmail.com>
2023-07-19 14:11:43 +0900

    fix: tabの大きさをsmに制限

--- src/Tab.elm
+++ src/Tab.elm
@@ -8,7 +8,7 @@ import Routes
 
 tabView : Html msg
 tabView =
-    div [ class "grid grid-cols-[auto,auto] gap-4" ] <|
+    div [ class "grid grid-cols-[auto,auto] gap-4 max-w-screen-sm" ] <|
         List.map
             (\( route, iconType ) ->
                 a [ Routes.href route, class "bg-slate-50 rounded-lg place-content-center grid py-4" ]
